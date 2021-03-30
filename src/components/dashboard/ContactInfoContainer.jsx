b27527805ee5b5b08ccf02133a14aca2ad432232
owenramire <owenomar29@gmail.com>
2021-03-30 13:16:04 -0600

    Add new items and use list
    I add my link to github with an icon and my username, also I notice that
    when react render my icon items in the ContactInfoContainer, i forgot to
    put a key on each item and the console send my a warning because React
    needs to know which component is rendering

--- src/components/dashboard/ContactInfoContainer.jsx
+++ src/components/dashboard/ContactInfoContainer.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ContactInfoItem from '../dashboard/ContactInfoItem';
 
 const ContactInfo = () => {
-  const userContactItems = ['Colima, col', 'owenomar29@gmail.com', 'owen omar ramirez lopez', '@owenramirez99'];
-  const userContactItemsIcons = ['faMapMarkedAlt', 'faEnvelope', 'faLinkedIn', 'faInstagram'];
+  const userContactItems = ['Colima, col', 'owenomar29@gmail.com', 'owen omar ramirez lopez', '@owenramire', '@owenramirez99'];
+  const userContactItemsIcons = ['faMapMarkedAlt', 'faEnvelope', 'faLinkedIn','faGitHub', 'faInstagram'];
 
   const displayUserContact = () => {
     return userContactItems.map((item, index) => {
       return (
-        <ContactInfoItem icon={userContactItemsIcons[index]} item={item}  />
+        <ContactInfoItem
+          key={index.toString()}
+          icon={userContactItemsIcons[index]}
+          item={item}
+        />
       );
     })
   }
