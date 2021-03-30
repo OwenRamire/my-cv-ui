a48c442c8573cf40ee5ee5f03dc04fd4b533b846
Owen Ramirez <42794917+OwenRamire@users.noreply.github.com>
2021-03-30 14:13:26 -0600

    Revert "Feature/refactor code"

--- src/components/dashboard/ContactInfoContainer.jsx
+++ src/components/dashboard/ContactInfoContainer.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import ContactInfoItem from '../dashboard/ContactInfoItem';
 
 const ContactInfo = () => {
-  const userContactItems = ['Colima, col', 'owenomar29@gmail.com', 'owen omar ramirez lopez', '@owenramire', '@owenramirez99'];
-  const userContactItemsIcons = ['faMapMarkedAlt', 'faEnvelope', 'faLinkedIn','faGitHub', 'faInstagram'];
+  const userContactItems = ['Colima, col', 'owenomar29@gmail.com', 'owen omar ramirez lopez', '@owenramirez99'];
+  const userContactItemsIcons = ['faMapMarkedAlt', 'faEnvelope', 'faLinkedIn', 'faInstagram'];
 
   const displayUserContact = () => {
     return userContactItems.map((item, index) => {
       return (
-        <ContactInfoItem
-          key={index.toString()}
-          icon={userContactItemsIcons[index]}
-          item={item}
-        />
+        <ContactInfoItem icon={userContactItemsIcons[index]} item={item}  />
       );
     })
   }
