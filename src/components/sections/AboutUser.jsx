43dfa5cda997e0359dbb126ab53a4d3027e7b736
owenramire <owenomar29@gmail.com>
2021-03-30 13:36:25 -0600

    Use props in sections components
    With the UserInfo file, i will pass the data of that file to these
    components to display in the browser

--- src/components/sections/AboutUser.jsx
+++ src/components/sections/AboutUser.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 const AboutUser = (props) => {
   return (
     <div className="about-user-container-introducing">
-      <h1>{props.title}</h1>
-      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-      Praesent blandit nibh at felis. Sed nec diam in dolor vestibulum
-      aliquet. Duis ullamcorper, nisi non facilisis molestie, lorem sem
-      aliquam nulla, id lacinia velit mi vestibulum enim.</p>
+      <p>{props.aboutUser}</p>
     </div>
   );
 }
--- src/components/sections/Sections.jsx
+++ src/components/sections/Sections.jsx
@@ -2,19 +2,49 @@ import React from 'react';
 import AboutUser from './AboutUser.jsx';
 import TitleSection from './TitleSection.jsx';
 import EducationExperienceSec from './EducationExperienceSec.jsx';
+import {
+  USER_ABOUT_ME,
+  USER_INFO_EXPERIENCE,
+  USER_EDUCATION,
+} from '../../utils/UserInfo';
 import '../../styles/css/sections/Sections.css';
 
 const Sections = () => {
+
+  const displayExperienceUser = () => {
+    return USER_INFO_EXPERIENCE.map((exp, i) => {
+      return (
+        <EducationExperienceSec
+          key={i.toString()}
+          institutionPosition={exp.institutionAndPosition}
+          date={exp.date}
+          description={exp.description }
+        />
+      );
+    });
+  }
+
+  const displayEducationUser = () => {
+    return USER_EDUCATION.map((education, i) => {
+      return (
+        <EducationExperienceSec
+          key={i.toString()}
+          institutionPosition={education.institutionAndPosition}
+          date={education.date}
+          description={education.description}
+        />
+      )
+    });
+  }
+
   return (
     <div className="sections-container">
       <TitleSection titleSection="About me" iconTitle="faUser" />
-      <AboutUser title="About me" />
-      
+      <AboutUser title="About me" aboutUser={USER_ABOUT_ME} />
       <TitleSection titleSection="Education" iconTitle="faUserGraduate" />
-      <EducationExperienceSec />
-      
+      {displayEducationUser()}
       <TitleSection titleSection="Experience" iconTitle="faMedal" />
-      <EducationExperienceSec />
+      {displayExperienceUser()}
     </div>
   );
 }
